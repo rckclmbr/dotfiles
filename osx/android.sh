5f4a985573a2178c9f9c91d589c89708eeb8c861
Josh Braegger <rckclmbr@gmail.com>
2014-12-19 01:06:07 -0700

    Moving android sdk to vendor

--- osx/android.sh
+++ osx/android.sh
@@ -1,4 +1,4 @@
-export ANDROID_HOME=/Applications/Android\ Studio.app/sdk/
+export ANDROID_HOME=$HOME/vendor/android-sdk-macosx
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
