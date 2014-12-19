ee21aeb7997f92d3dc540d2988e53039dfa6cc46
Josh Braegger <rckclmbr@gmail.com>
2014-12-18 23:42:03 -0700

    OSX compatibility

--- appengine/appengine.sh
+++ appengine/appengine.sh
@@ -1,5 +1,10 @@
 
-export PATH=~/google-cloud-sdk/bin/:~/go_appengine:$PATH
-
+export PATH=$PATH:~/go_appengine
 source ~/google-cloud-sdk/path.bash.inc
-source ~/google-cloud-sdk/completion.bash.inc
+
+if [[ -z "${ZSH+x}" ]];
+then
+    source ~/google-cloud-sdk/completion.bash.inc
+else
+    source ~/google-cloud-sdk/completion.zsh.inc
+fi
