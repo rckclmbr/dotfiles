5eac10b5734baea28e57d4c74da9f1ccdbd78a43
Josh Braegger <rckclmbr@gmail.com>
2014-12-19 01:01:07 -0700

    Updating aws to vendor dir

--- aws/aws.sh
+++ aws/aws.sh
@@ -1,5 +1,5 @@
-export EC2_HOME=~/aws/ec2-api-tools-1.5.5.0/
-export AWS_ELB_HOME=~/aws/ElasticLoadBalancing-1.0.17.0/
+export EC2_HOME=~/vendor/ec2-api-tools-1.5.5.0/
+export AWS_ELB_HOME=~/vendor/ElasticLoadBalancing-1.0.17.0/
 
 export PATH=$PATH:$EC2_HOME/bin
 export PATH=$PATH:$AWS_ELB_HOME/bin
