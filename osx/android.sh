79d04c1b894ed7d425c48435d354ff4091c04cf9
Josh Braegger <rckclmbr@gmail.com>
2014-12-19 01:16:04 -0700

    No need for gradle

--- osx/android.sh
+++ osx/android.sh
@@ -1,6 +1,3 @@
 export ANDROID_HOME=$HOME/vendor/android-sdk-macosx
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
-
-export GRADLE_HOME=/Users/jbraegger/java/gradle-1.6
-export PATH=$PATH:$GRADLE_HOME/bin
