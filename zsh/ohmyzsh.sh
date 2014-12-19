413bdfa89a7f067585ce87be408c50b4a9236e6d
Josh Braegger <rckclmbr@gmail.com>
2014-12-18 23:30:33 -0700

    OSX, and move script dir

---
+++ zsh/ohmyzsh.sh
@@ -0,0 +1,27 @@
+ZSH=$HOME/.oh-my-zsh
+
+# ~/.oh-my-zsh/themes/ or "random"
+#ZSH_THEME="apple"
+ZSH_THEME="agnoster"
+
+# Set to this to use case-sensitive completion
+# CASE_SENSITIVE="true"
+
+# Comment this out to disable weekly auto-update checks
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment following line if you want to disable colors in ls
+# DISABLE_LS_COLORS="true"
+
+# Uncomment following line if you want to disable autosetting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment following line if you want red dots to be displayed while waiting for completion
+# COMPLETION_WAITING_DOTS="true"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+plugins=(git textmate python osx)
+
+source $ZSH/oh-my-zsh.sh
