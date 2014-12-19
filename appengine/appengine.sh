4c8456bc3890f46e5f48e5e0295212c9a69c06c6
Josh Braegger <rckclmbr@gmail.com>
2014-12-19 00:16:37 -0700

    Updating for zsh on ubuntu

--- appengine/appengine.sh
+++ appengine/appengine.sh
@@ -1,10 +1,13 @@
 
 export PATH=$PATH:~/go_appengine
-source ~/google-cloud-sdk/path.bash.inc
 
-if [[ -z "${ZSH+x}" ]];
+if [[ -d ~/google-cloud-sdk ]];
 then
-    source ~/google-cloud-sdk/completion.bash.inc
-else
-    source ~/google-cloud-sdk/completion.zsh.inc
+    if [[ -z "${ZSH+x}" ]];
+    then
+        source ~/google-cloud-sdk/path.bash.inc
+        source ~/google-cloud-sdk/completion.bash.inc
+    else
+        source ~/google-cloud-sdk/completion.zsh.inc
+    fi
 fi
