15f77d184a5b88047ac4f67593a9ef098d47d4b3
Josh Braegger <rckclmbr@gmail.com>
2014-12-23 22:02:26 -0700

    Moving android, adding i3status

--- android/path.sh
+++ android/path.sh
@@ -1,3 +1,3 @@
-export ANDROID_HOME=$HOME/vendor/android-sdk-macosx
+export ANDROID_HOME=$HOME/vendor/android-sdk
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
